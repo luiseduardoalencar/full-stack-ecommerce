f6f4bde01f8c16bcbcc5239a9de44c818477454c
Luis Eduardo <luiseduardoalencarmelo@gmail.com>
2025-01-07 15:54:22 -0300

    first commit

---
+++ services/config-server/src/test/java/com/br/pi/personal/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.pi.personal.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
